90be976a2a7670dedb9c4b4130bfad73f052d924
hiepdeptrai0908 <97216593+hiepdeptrai0908@users.noreply.github.com>
2022-04-25 16:10:31 +0900

    shop ao dai

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var indexProductShows = document.querySelectorAll('.home-product-item')
+var indexName = document.querySelector('.home-product-item__name')
+console.log(indexName)
+
+// for(var i = 7; i < indexProductShows.length; ++i) {
+//     indexProductShows[i].onclick = function(e) {
+//         this.href = './product/product2.html'
+//     }
+// }
+
